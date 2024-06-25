4d276d157aa6ac700ae5df63f27ba19ac2aa45ad
Lídia Carvalho <lidiafcarvalho@outlook.com>
2024-06-25 20:47:04 -0300

    Alterações iniciais: criando os componentes da homepage.

---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Home.css"
+import NavBar from './navegacao/NavBar'
+import Feed from './feed/Feed'
+
+function Home() {
+  return (
+    <div className='home'>
+        <div className='home_nav'>
+            <NavBar></NavBar>
+        </div>
+
+        <div className='home_feed'>
+            <Feed></Feed>
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
